01a3609b6fc9feb28da021eb0b1fffcf7b0701c8
Joe Howley <joe.howley88@gmail.com>
2024-10-09 11:40:25 +0000

    install crispy forms

---
+++ cniapp/forms.py
@@ -0,0 +1,8 @@
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelsForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
--- cniapp/views.py
+++ cniapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Review
-
+from .forms import CommentForm
 # Create your views here.
 
 class Reviews(generic.ListView):
